737787f338c26c1986dfda853306bf5b6dac4cf7
Kosaka Bun <kosaka-bun@qq.com>
2023-12-16 23:55:47 +0800

    防止WebView在屏幕旋转后自动刷新

--- app/src/main/java/de/honoka/android/h5apptemplate/WebActivity.kt
+++ app/src/main/java/de/honoka/android/h5apptemplate/WebActivity.kt
@@ -3,9 +3,13 @@ package de.honoka.android.h5apptemplate
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +26,78 @@ class WebActivity : AppCompatActivity() {
     private var firstWebActivity: Boolean = false
 
     private lateinit var webView: WebView
+
+    private val webViewClient = object : WebViewClient() {
+
+        //重写此方法，解决WebView在重定向时打开系统浏览器的问题
+        override fun shouldOverrideUrlLoading(
+            view: WebView,
+            request: WebResourceRequest
+        ): Boolean {
+            val url = request.url.toString()
+            //禁止WebView加载未知协议的URL
+            if(!url.startsWith("http")) return true
+            view.loadUrl(url)
+            return true
+        }
+    }
+
+    private val webChromeClient = object : WebChromeClient() {
+
+        override fun onShowCustomView(view: View, callback: CustomViewCallback) {
+            fullScreenView?.run {
+                callback.onCustomViewHidden()
+                return
+            }
+            val decorView = window.decorView as FrameLayout
+            decorView.addView(view, fullScreenViewParams)
+            showStatusBar(false)
+            fullScreenView = view
+            fullScreenViewCallBack = callback
+        }
+
+        override fun onHideCustomView() {
+            fullScreenView ?: return
+            showStatusBar()
+            val decorView = window.decorView as FrameLayout
+            decorView.removeView(fullScreenView)
+            fullScreenView = null
+            fullScreenViewCallBack?.onCustomViewHidden()
+            webView.visibility = View.VISIBLE
+        }
+    }
+
+    private var fullScreenView: View? = null
+
+    private val fullScreenViewParams = FrameLayout.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.MATCH_PARENT
+    )
+
+    private var fullScreenViewCallBack: WebChromeClient.CustomViewCallback? = null
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+
+        private var lastTimePressBack = 0L
+
+        override fun handleOnBackPressed() {
+            if(webView.canGoBack()) {
+                webView.goBack()
+                return
+            }
+            if(!firstWebActivity) {
+                finish()
+                return
+            }
+            if(System.currentTimeMillis() - lastTimePressBack > 2500) {
+                Toast.makeText(this@WebActivity, "再进行一次返回退出应用", Toast.LENGTH_SHORT).show()
+                lastTimePressBack = System.currentTimeMillis()
+            } else {
+                finish()
+                if(firstWebActivity) exitProcess(0)
+            }
+        }
+    }
 
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,20 +122,8 @@ class WebActivity : AppCompatActivity() {
 
     private fun initWebView() {
         webView = findViewById<WebView>(R.id.web_view).apply {
-            webViewClient = object : WebViewClient() {
-
-                //重写此方法，解决WebView在重定向时打开系统浏览器的问题
-                override fun shouldOverrideUrlLoading(
-                    view: WebView,
-                    request: WebResourceRequest
-                ): Boolean {
-                    val url = request.url.toString()
-                    //禁止WebView加载未知协议的URL
-                    if(!url.startsWith("http")) return true
-                    view.loadUrl(url)
-                    return true
-                }
-            }
+            webViewClient = this@WebActivity.webViewClient
+            webChromeClient = this@WebActivity.webChromeClient
             settings.run {
                 //必须打开，否则网页可能显示为空白
                 javaScriptEnabled = true
@@ -70,28 +134,7 @@ class WebActivity : AppCompatActivity() {
             scrollBarStyle = View.SCROLLBARS_OUTSIDE_OVERLAY
             loadUrl(this@WebActivity.url)
         }
-        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
-
-            private var lastTimePressBack = 0L
-
-            override fun handleOnBackPressed() {
-                if(webView.canGoBack()) {
-                    webView.goBack()
-                    return
-                }
-                if(!firstWebActivity) {
-                    finish()
-                    return
-                }
-                if(System.currentTimeMillis() - lastTimePressBack > 2500) {
-                    Toast.makeText(this@WebActivity, "再进行一次返回退出应用", Toast.LENGTH_SHORT).show()
-                    lastTimePressBack = System.currentTimeMillis()
-                } else {
-                    finish()
-                    if(firstWebActivity) exitProcess(0)
-                }
-            }
-        })
+        onBackPressedDispatcher.addCallback(onBackPressedCallback)
     }
 
     @SuppressLint("JavascriptInterface")
@@ -99,5 +142,11 @@ class WebActivity : AppCompatActivity() {
         getAllJsInterfaces(webView).forEach {
             webView.addJavascriptInterface(it, "android_${it.javaClass.simpleName}")
         }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun showStatusBar(show: Boolean = true) {
+        val flag = if(show) 0 else WindowManager.LayoutParams.FLAG_FULLSCREEN
+        window.setFlags(flag, WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
