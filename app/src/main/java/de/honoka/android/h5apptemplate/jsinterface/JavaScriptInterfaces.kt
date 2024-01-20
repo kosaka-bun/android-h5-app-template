7d9e76a5b4a948153c9497051b5561dd4cac95af
Kosaka Bun <kosaka-bun@qq.com>
2024-01-20 16:38:11 +0800

    更改AsyncTaskJsInterface被添加的位置

--- app/src/main/java/de/honoka/android/h5apptemplate/jsinterface/JavaScriptInterfaces.kt
+++ app/src/main/java/de/honoka/android/h5apptemplate/jsinterface/JavaScriptInterfaces.kt
@@ -6,12 +6,12 @@ import de.honoka.android.h5apptemplate.ui.WebActivity
 
 class JavaScriptInterfaces(private val webActivity: WebActivity) {
 
-    private val interfaceInstances = arrayOf(
-        BasicJsInterface(webActivity),
-        AsyncTaskJsInterface(this, webActivity)
+    private val interfaceInstances = arrayListOf<Any>(
+        BasicJsInterface(webActivity)
     )
 
     val interfaces = HashMap<String, Any>().apply {
+        interfaceInstances.add(AsyncTaskJsInterface(this@JavaScriptInterfaces, webActivity))
         interfaceInstances.forEach {
             this[it.javaClass.simpleName] = it
         }
